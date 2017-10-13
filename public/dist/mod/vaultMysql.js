6454e94da1f3fe3f1e43d4e15c24bd89f8f725ac
Renan Berto <renanbertoo@gmail.com>
2017-10-13 19:19:52 -0300

    Format HTML into js

--- public/dist/mod/vaultMysql.js
+++ public/dist/mod/vaultMysql.js
@@ -10,6 +10,15 @@ $('#mysqlForm')
           prompt: 'Please enter github token.'
         }
       ]
+    },
+    checkbox: {
+      identifier: 'accessMode',
+      rules: [
+        {
+          type   : 'checked',
+          prompt : 'Please enter access mode.'
+        }
+      ]
     }
   }
 });
@@ -22,14 +31,33 @@ $('#submitMysqlForm').on('click', function() {
       .addClass('loading')
       .siblings()
       .removeClass('loading');
-      
+  
     var options = {
       dataType: 'json',
       success: function(response) {
-        var myData = JSON.stringify(response);
-        var myDataObject = JSON.parse(myData);
-        var info = 'Username: <code>' + myDataObject.username + '</code></br> Password: <code>' + myDataObject.password + '</code>';
+        var Data = JSON.stringify(response);
+        var DataObject = JSON.parse(Data);
         
+        if (DataObject.respError != null) {
+          var myErrorResponse = JSON.stringify(DataObject.respError);
+          var myErrorResponseObject = JSON.parse(myErrorResponse);
+          var info = '<h3>Error: </h3>' + myErrorResponseObject
+          
+        } else {
+          var info =
+            '<div class="header"> Username: </div>\n' +
+            '    <li class="list">' + DataObject.username + '</li>' +
+            '</div> </br>\n' +
+  
+            '<div class="header"> Password: </div>\n' +
+            '    <li class="list">' + DataObject.password + '</li>' +
+            '</div> </br>\n' +
+  
+            '<div class="header"> Tempo de duração: </div>\n' +
+            '    <li class="list">' + DataObject.lease_time + ' seconds </li>' +
+            '</div>\n'
+          ;
+        }
         $("#mysqlCredsInformation").append(info);
         $('.ui.modal')
         .modal('setting', 'transition', 'browse')
