afcdaa07b4df33b57aebb14d3828c1f55d1e8d06
Renan Berto <renan.berto@autocargo.com.br>
2017-10-14 17:10:06 -0300

    Update history outage with new extension

--- history/outages.go
+++ history/outages.go
@@ -7,7 +7,7 @@ import (
 
 //HTMLOutagesHandler returns a html page with all outage history in a given period
 func HTMLOutagesHandler(c *gin.Context){
-	c.HTML(http.StatusOK, "history_outage.tmpl", gin.H{
+	c.HTML(http.StatusOK, "history_outage.html", gin.H{
 		"title": "Agile Promoter Operations Center",
 	})
 }
