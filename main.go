465ba0ebee85d78bd3ff201ae1107ec1d8687818
Renan Berto <renan.berto@autocargo.com.br>
2017-10-14 17:11:14 -0300

    Added new HTML render and added mongoDB connection

--- main.go
+++ main.go
@@ -5,12 +5,20 @@ import (
   "net/http"
   "github.com/renanberto/apocV2/history"
   "github.com/renanberto/apocV2/vault"
-)
+  "github.com/renanberto/apocV2/utils"
+  "github.com/renanberto/apocV2/ginHtmlRender"
+ )
+
+func init() {
+  utils.Mongoconnect()
+}
 
 func main() {
   router := gin.Default()
-  router.LoadHTMLGlob("templates/*")
-  router.Static("/dist", "public/dist")
+  router.Use(utils.Connect)
+  router.Use(utils.ErrorHandler)
+
+  htmlSettings(router)
 
   // Posts
   router.POST("/vault/mysql/create-creds", vault.InputMysqlHandler)
@@ -24,14 +32,25 @@ func main() {
   router.Run(":4000")
 }
 
+func htmlSettings(router *gin.Engine){
+  htmlRender := ginHtmlRender.New()
+  htmlRender.Debug = gin.IsDebugging()
+  htmlRender.Layout = "index"
+  router.HTMLRender = htmlRender.Create()
+  router.RedirectTrailingSlash = true
+  router.RedirectFixedPath = true
+  router.LoadHTMLGlob("templates/*")
+  router.Static("/dist", "public/dist")
+}
+
 func HTMLIndexHandler(c *gin.Context) {
-  c.HTML(http.StatusOK, "index.tmpl", gin.H{
+  c.HTML(http.StatusOK, "index.html", gin.H{
     "title": "Agile Promoter Operations Center",
   })
 }
 
 func HTMLAboutHandler(c *gin.Context) {
-  c.HTML(http.StatusOK, "about.tmpl", gin.H{
+  c.HTML(http.StatusOK, "about.html", gin.H{
     "title": "Agile Promoter Operations Center",
   })
 }
