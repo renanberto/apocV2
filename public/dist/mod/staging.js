497c8f8206d92636aa05aa394f774c57c9cb00fc
Renan Berto <renanbertoo@gmail.com>
2017-10-17 21:03:32 -0200

    Added new JS to staging environment

---
+++ public/dist/mod/staging.js
@@ -0,0 +1,16 @@
+$('.ui.red.button').on('click', function() {
+  var ID = $(this).find("input").attr('value');
+  
+  var formData = new FormData();
+  formData.append("id", ID);
+  var request = new XMLHttpRequest();
+  request.open("POST", "/v1/staging/environment/remove");
+  request.send(formData);
+  
+  setTimeout(location.reload.bind(location), 3000);
+  
+  $(this)
+  .addClass('loading')
+  .siblings()
+  .removeClass('loading');
+});
