dc4bd9ee0d3ba34e4d9c4726a6c8eaed2a9c47df
Renan Berto <renanbertoo@gmail.com>
2017-10-17 21:04:47 -0200

    Added remove function

--- staging/environment.go
+++ staging/environment.go
@@ -4,36 +4,73 @@ import (
 	"net/http"
 	"github.com/gin-gonic/gin"
 	"github.com/renanberto/apocV2/utils"
+	"strings"
+	"time"
 	"fmt"
 )
+
+type ContainerInformation struct {
+	ID	string
+	Name	string
+	Status	string
+	State	string
+	Created	time.Time
+}
 
 func HTMLStagingHandler(c *gin.Context) {
 
 	listedStagingContainers := listAllStagingContainers()
-	for _,i := range listedStagingContainers {
-
-		fmt.Println(i)
-	}
 
 	c.HTML(http.StatusOK, "staging.html", gin.H{
 		"title": "Agile Promoter Operations Center",
+		"stagingContainers": &listedStagingContainers,
 	})
 }
 
-func listAllStagingContainers() (listedStagingContainers []string){
-	  var tmpDockerConnection utils.DockerConnection
-	  var stagingNames []string
+func listAllStagingContainers() []ContainerInformation {
+	  var newDockerConnection utils.DockerConnection
+		var stagingContainersInformation []ContainerInformation
+		var ci ContainerInformation
 
-	  tmpDockerConnection.New("involves-hetfield")
+		newDockerConnection.New("involves-hetfield")
 
-		err, allStagingContainers := utils.ListAllContainers(tmpDockerConnection)
+		err, stagingContainers := utils.ListAllContainers(newDockerConnection)
 		if err != nil {panic(err)}
 
-		for _, element := range allStagingContainers {
-			for _, name := range element.Names {
-				stagingNames = append(stagingNames,name)
+		for _, element := range stagingContainers {
+			joinContainerName := strings.Join(element.Names,"")
+			ContainerNameWithReplace := strings.Replace(joinContainerName,"/","",1)
+			t := time.Unix(element.Created,0)
+			if strings.Contains(ContainerNameWithReplace,"staging_qa_") {
+				ci.ID = element.ID
+				ci.Name = ContainerNameWithReplace
+				ci.Status = element.Status
+				ci.Created = t
+				ci.State = element.State
+				stagingContainersInformation = append(stagingContainersInformation,ci)
 			}
 		}
 
-	return stagingNames
+	return stagingContainersInformation
+}
+
+func RemoveStagingContainer(c *gin.Context) {
+	var newDockerConnection utils.DockerConnection
+
+	containerID := c.PostForm("id")
+	newDockerConnection.New("involves-hetfield")
+	err := utils.RemoveContainerId(containerID,newDockerConnection)
+
+	fmt.Println("passou")
+
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"error": "Error",
+		})
+	} else {
+		c.JSON(http.StatusOK, gin.H{
+			"success": "success",
+		})
+	}
+
 }
